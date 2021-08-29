7f0231479013c9074250e211a551e5bef29bdbe8
artemghostartem <artemghostartem@gmail.com>
2021-08-29 07:47:35 +0300

    Fix

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,26 @@ window.$ = $
 document.addEventListener('DOMContentLoaded', () => {
 	
 	// Custom JS
+// popup
+function pop() {
+		$('.popup-with-form').magnificPopup({
+			type: 'inline',
+			preloader: false,
+			focus: '#name',
+	
+			// When elemened is focused, some mobile browsers in some cases zoom in
+			// It looks not nice, so we disable it:
+			callbacks: {
+				beforeOpen: function() {
+					if($(window).width() < 700) {
+						this.st.focus = false;
+					} else {
+						this.st.focus = '#name';
+					}
+				}
+			}
+		});
+	};
 //marquee
 	function animateMarquee(el, duration) {
 	const innerEl = el.querySelector('.marquee__inner');
