8420cd1fc53cd1036f96a855f37d0b603d2c82bf
artemghostartem <artemghostartem@gmail.com>
2021-08-31 03:55:44 +0300

    update

--- app/js/app.js
+++ app/js/app.js
@@ -2,64 +2,69 @@
 import $ from 'jquery'
 window.jQuery = $
 window.$ = $
+import "magnific-popup"
 
 // // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
+require('~/app/js/jquery.malihu.PageScroll2id.min.js')
 
 document.addEventListener('DOMContentLoaded', () => {
-	
-	// Custom JS
-// popup
-function pop() {
-		$('.popup-with-form').magnificPopup({
-			type: 'inline',
-			preloader: false,
-			focus: '#name',
-	
-			// When elemened is focused, some mobile browsers in some cases zoom in
-			// It looks not nice, so we disable it:
-			callbacks: {
-				beforeOpen: function() {
-					if($(window).width() < 700) {
-						this.st.focus = false;
-					} else {
-						this.st.focus = '#name';
-					}
-				}
-			}
-		});
-	};
-//marquee
-	function animateMarquee(el, duration) {
-	const innerEl = el.querySelector('.marquee__inner');
-	const innerWidth = innerEl.offsetWidth;
-	const cloneEl = innerEl.cloneNode(true);
-	el.appendChild(cloneEl);
-
-	let start = performance.now();
-	let progress;
-	let translateX;
-
-	requestAnimationFrame(function step(now) {
-		progress = (now - start) / duration;
-
-		if (progress > 1) {
-			progress %= 1;
-			start = now;
-		}
-
-		translateX = innerWidth * progress;
-
-		innerEl.style.transform = `translate3d(-${translateX}px, -40px , 0)`;
-		cloneEl.style.transform = `translate3d(-${translateX}px, -40px , 0)`;
-		requestAnimationFrame(step);
+	$("a[href*='#']").mPageScroll2id({
+		offset: 100
 	});
-}
+	// Custom JS
+	// popup
 
-const marquee1 = document.querySelector('#marquee1');
-const marquee2 = document.querySelector('#marquee2');
+	let popupDiv = $('.popup-with-move-anim')
+	popupDiv.magnificPopup({
+		type: 'inline',
 
-animateMarquee(marquee1, 30000);
-animateMarquee(marquee2, 30000);
+		fixedContentPos: true,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+	let popupCloseButton = document.querySelector('.signup .close-button')
+	popupCloseButton.addEventListener('click', () => {
+		popupDiv.close();
+	})
+	//marquee
+	function animateMarquee(el, duration) {
+		const innerEl = el.querySelector('.marquee__inner');
+		const innerWidth = innerEl.offsetWidth;
+		const cloneEl = innerEl.cloneNode(true);
+		el.appendChild(cloneEl);
+
+		let start = performance.now();
+		let progress;
+		let translateX;
+
+		requestAnimationFrame(function step(now) {
+			progress = (now - start) / duration;
+
+			if (progress > 1) {
+				progress %= 1;
+				start = now;
+			}
+
+			translateX = innerWidth * progress;
+
+			innerEl.style.transform = `translate3d(-${translateX}px, -40px , 0)`;
+			cloneEl.style.transform = `translate3d(-${translateX}px, -40px , 0)`;
+			requestAnimationFrame(step);
+		});
+	}
+
+	const marquee1 = document.querySelector('#marquee1');
+	const marquee2 = document.querySelector('#marquee2');
+
+	animateMarquee(marquee1, 30000);
+	animateMarquee(marquee2, 30000);
 
 })
